686cd5d9821c5ff621c40601e4f57282997875d8
Your Name <you@example.com>
2021-11-28 20:37:30 +0800

    增加计时

--- run.py
+++ run.py
@@ -10,12 +10,13 @@ def file_name(file_dir):
     return lst
 
 pathList = file_name("/home/shbing/dataUse/datasets/bipartite/")
-for path in pathList:
-    oriPath = path
-    newPath = path.replace("dataUse", "datasetsNew")
-    #print(oriPath, newPath)
-    name = newPath.split("/")[-2]
-    out = os.system(f"./butterfly.bin {newPath} run -1 {name} 32")
+
+# for path in pathList:
+#     oriPath = path
+#     newPath = path.replace("dataUse", "datasetsNew")
+#     #print(oriPath, newPath)
+#     name = newPath.split("/")[-2]
+#     out = os.system(f"./butterfly.bin {newPath} run -1 {name} 32")
 # for path in pathList:
 #     oriPath = path
 #     newPath = path.replace("dataUse", "datasetsNew")
